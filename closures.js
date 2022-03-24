703f0f32eda81d49c1ca6dd82991480b3b4a04a6
Ryan Ku <rjhk@uw.edu>
2022-03-24 09:46:04 -0700

    inclass excercises

--- closures.js
+++ closures.js
@@ -20,7 +20,16 @@ console.log(counter.getCount());
 // - addMessage method that adds a message to the array
 // - getMessage(index) method that returns the message at index index
 
-const createMessageHolder = () => {};
+const createMessageHolder = () => {
+  const messageArray = [];
+  const addMessage = message => messageArray.push(message);
+  const getMessage = index => messageArray[index];
+
+  return {
+    addMessage,
+    getMessage
+  };
+};
 
 // Test
 const messageHolder = createMessageHolder();
@@ -55,7 +64,12 @@ console.log(addThree(41));
 // This will return a function a function greet
 // - This accepts a single argument, name (i.e. "Matt")
 // - This function should return the greeting combined with the name, (i.e. "Hello Matt")
-const createGreeting = function(greeting) {};
+const createGreeting = function(greeting) {
+  const greet = function(name) {
+    return `${greeting}, ${name}`;
+  }
+  return greet;
+};
 
 // Test
 const welcomeGreet = createGreeting('Welcome');
